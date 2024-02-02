ef2a9d3ed64712cc88aceb61a77d688567086409
Kyle Palermo <61397896+kylespalermo@users.noreply.github.com>
2024-02-02 09:59:01 -0500

    Set up filtering

--- src/components/partsDirectory.vue
+++ src/components/partsDirectory.vue
@@ -1,10 +1,18 @@
 <template>
   <section>
     <h1>Ronslist</h1>
+    
+    <fieldset v-for="filter in this.filters" :key="filter">
+      <legend>{{ filter }}</legend>      
+      <div v-for="filterValue in this.filterValues[filter]" :key="filterValue" @click="setActiveFilters(filter, filterValue)">
+        <input type="checkbox" :id="filterValue" :name="filterValue">
+        <label>{{ filterValue }}</label>
+      </div>
+    </fieldset>
     <ul>
-        <li v-for="(part, index) in this.parts" :key="index" :class="[`${index}`]">{{part["Fastener Type"]}}
+        <li v-for="part in filterItems" :key="part['MFG PN']">{{part["Manufacturer"] + " " + part["MFG PN"]}}
             <ul>
-                <li v-for="(specValue, specKey) in part" :key="specValue['Web Link']">{{specKey}}:{{specValue}}</li>
+                <li v-for="(specValue, specKey) in part" :key="specValue['Web Link']">{{specKey}}: {{specValue}}</li>
             </ul>
         </li>
     </ul>
@@ -15,6 +23,12 @@
 
 
 <script>
+
+//need to build in and/or logic for filter groups, add additional control that tells it which json to render. 
+//hard code the top-level keys for each item group
+//consider the experience of revealing additional filters as you narrow your search
+
+
 // Importing array from our JSON file
 import json from "@/assets/fasteners.json";
 
@@ -22,19 +36,79 @@ export default {
   name: "PartsDirectory",
   data: function() {
     return {
-      parts: json // passing array data into Vue
+      parts: json, // passing array data into Vue
+      filters: [
+        "Primary Use",
+        "Manufacturer",
+        "Head Type"
+      ],
+      filterValues: {},
+      activeFilters: {}
     };
   },
-// this is how you filter, just need to loop through all the values of each property and create like a dropdown for each or something
-//then you render the list with the computed property, not the base data
-//although I think you should actually use a method here?
-//then you pass the variable via v-on:click="filterItems(etc etc)""
-//   computed: {
-//     filterItems() {
-//       return data.filter(obj => {
-//             return obj.[key] === value;
-//         })
-//     }
-//   }
-};
+  mounted() {
+      this.filters.forEach(filter => {
+        let listOfValues = []
+        this.parts.forEach(part => {
+          listOfValues.push(part[filter])
+        })
+        this.filterValues[filter] = [...new Set(listOfValues)]
+        this.activeFilters[filter] = [];
+      })
+      return undefined
+    },
+  computed: {
+    filterItems() {
+      let filteredItems = [];
+      let filtersApplied = 0;
+      for (let activeFilterCat in this.activeFilters) {
+        filtersApplied += this.activeFilters[activeFilterCat].length;
+      }
+      if (filtersApplied > 0) {
+        this.parts.forEach(part => {
+          for (let activeFilterCat in this.activeFilters) {
+            this.activeFilters[activeFilterCat].forEach(activeFilter => {
+              if (Object.values(part).includes(activeFilter)) {
+                filteredItems.push(part)
+              }
+            })
+          }
+        })
+        return [...new Set(filteredItems)];
+        } else {
+          return this.parts;
+      }
+    },
+  },
+  methods: {
+    setActiveFilters(filterCat, filterValue) {
+      this.filtered = true;
+      if (!this.activeFilters[filterCat].includes(filterValue)) {
+          this.activeFilters[filterCat].push(filterValue)
+        } else {
+          let index = this.activeFilters[filterCat].indexOf(filterValue);
+            if (index > -1) { // only splice array when item is found
+            this.activeFilters[filterCat].splice(index, 1); // 2nd parameter means remove one item only
+          }
+        }
+      }
+    },
+  }
 </script>
+
+<style>
+
+ul ul {
+  margin-bottom: 20px;
+}
+
+.filters {
+  list-style: none;
+  padding: 0;
+}
+
+.active {
+  color: red;
+}
+
+</style>
