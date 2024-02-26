1b7aa0915a8b648f8628b2999880de7b17ab417e
Kyle Palermo <61397896+kylespalermo@users.noreply.github.com>
2024-02-25 19:49:40 -0500

    finishing prototype

--- src/components/partsDirectory.vue
+++ src/components/partsDirectory.vue
@@ -1,16 +1,19 @@
 <template>
   <section>
     <h1>Ronslist</h1>
-    
-    <fieldset v-for="filter in this.filters" :key="filter">
-      <legend>{{ filter }}</legend>      
-      <div v-for="filterValue in this.filterValues[filter]" :key="filterValue" @click="setActiveFilters(filter, filterValue)">
-        <input type="checkbox" :id="filterValue" :name="filterValue">
-        <label>{{ filterValue }}</label>
-      </div>
-    </fieldset>
+    <input type="text" v-model="searchQuery">
+    <div class="fieldset-wrapper">
+      <fieldset v-for="filter in this.filters" :key="filter">
+        <legend>{{ filter }}</legend>      
+        <div v-for="filterValue in this.filterValues[filter]" :key="filterValue" @click="setActiveFilters(filter, filterValue)">
+          <input type="checkbox" :id="filterValue" :name="filterValue">
+          <label>{{ filterValue }}</label>
+        </div>
+      </fieldset>
+    </div>
     <ul>
-        <li v-for="part in filterItems" :key="part['MFG PN']">{{part["Manufacturer"] + " " + part["MFG PN"]}}
+        <li v-for="part in searchItems" :key="part['MFG PN']">{{part["Manufacturer"] + " " + part["MFG PN"]}}
+          {{ console.log(part.item) }}
             <ul>
                 <li v-for="(specValue, specKey) in part" :key="specValue['Web Link']">{{specKey}}: {{specValue}}</li>
             </ul>
@@ -18,9 +21,6 @@
     </ul>
   </section>
 </template>
-
-
-
 
 <script>
 
@@ -30,6 +30,7 @@
 
 
 // Importing array from our JSON file
+import Fuse from 'fuse.js';
 import json from "@/assets/fasteners.json";
 
 export default {
@@ -43,10 +44,11 @@ export default {
         "Head Type"
       ],
       filterValues: {},
-      activeFilters: {}
+      activeFilters: {},
+      searchQuery: ''
     };
   },
-  mounted() {
+  mounted(){
       this.filters.forEach(filter => {
         let listOfValues = []
         this.parts.forEach(part => {
@@ -55,10 +57,29 @@ export default {
         this.filterValues[filter] = [...new Set(listOfValues)]
         this.activeFilters[filter] = [];
       })
-      return undefined
+
+      const fuseOptions = {
+        isCaseSensitive: false,
+        includeScore: false,
+        // shouldSort: true,
+        includeMatches: false,
+        findAllMatches: false,
+        // minMatchCharLength: 1,
+        // location: 0,
+        threshold: 0.2,
+        // distance: 100,
+        // useExtendedSearch: false,
+        // ignoreLocation: false,
+        // ignoreFieldNorm: false,
+        // fieldNormWeight: 1,
+        keys: Object.keys(this.parts[0])
+      };
+
+      this.fuse = new Fuse(this.filterItems, fuseOptions)
+      return undefined;
     },
   computed: {
-    filterItems() {
+    filterItems(){
       let filteredItems = [];
       let filtersApplied = 0;
       for (let activeFilterCat in this.activeFilters) {
@@ -79,6 +100,17 @@ export default {
           return this.parts;
       }
     },
+    searchItems(){
+        if (this.searchQuery.length > 2) {
+        let results = this.fuse.search(this.searchQuery);
+        console.log(results)
+        let newResults = []
+        results.forEach(result =>{newResults.push(result.item)})//fuse returning weird structure, this fixes temporarily
+        return newResults
+        } else {
+          return this.filterItems;
+      }
+    }
   },
   methods: {
     setActiveFilters(filterCat, filterValue) {
@@ -97,6 +129,11 @@ export default {
 </script>
 
 <style>
+
+.fieldset-wrapper {
+  display: flex;
+  flex-direction: row;
+}
 
 ul ul {
   margin-bottom: 20px;
